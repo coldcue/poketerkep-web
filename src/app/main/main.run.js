21b273a02286201f8f90ade8db8932689aeb15aa
Tamas Kinsztler <kinsztlert@sonrisa.hu>
2016-08-11 17:41:54 +0200

    #65 - Ignore loading on facebook.com calls

--- src/app/main/main.run.js
+++ src/app/main/main.run.js
@@ -44,8 +44,9 @@ function MainRun($rootScope, $state, $locale, ENV, MockService, amMoment, Loader
     $locale.NUMBER_FORMATS.GROUP_SEP = ' ';
 
     // Ignore endpoints in loader interceptor
+    LoaderInterceptor.addIgnoredEndpoint('https://www.facebook.com');
     LoaderInterceptor.addIgnoredEndpoint(ENV.apiEndpoint + 'game');
-
+    
     // Send a page view request to analytics
     Analytics.pageView();
 
